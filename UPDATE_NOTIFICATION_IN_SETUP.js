2438169c6d84266122079091f242e2fbfb4f561c
Amateur Audio Dude <168192910+AmateurAudioDude@users.noreply.github.com>
2025-05-04 21:28:03 +1000

    Add files via upload

---
+++ UPDATE_NOTIFICATION_IN_SETUP.js
@@ -0,0 +1,91 @@
+// Variables
+
+const pluginVersion = '1.0.0';
+const pluginName = "My Plugin";
+const pluginHomepageUrl = "https://github.com/Plugin_URL";
+const pluginUpdateUrl = "https://example.com/version.txt";
+const pluginSetupOnlyNotify = true;
+const CHECK_FOR_UPDATES = true;
+
+
+
+
+// Function for update notification in /setup
+function checkUpdate(setupOnly, pluginVersion, pluginName, urlUpdateLink, urlFetchLink) {
+    if (setupOnly && window.location.pathname !== '/setup') return;
+
+    // Function to check for updates
+    async function fetchFirstLine() {
+        const urlCheckForUpdate = urlFetchLink;
+
+        try {
+            const response = await fetch(urlCheckForUpdate);
+            if (!response.ok) {
+                throw new Error(`[${pluginName}] update check HTTP error! status: ${response.status}`);
+            }
+
+            const text = await response.text();
+            const firstLine = text.split('\n')[0]; // Extract first line
+
+            const version = firstLine;
+
+            return version;
+        } catch (error) {
+            console.error(`[${pluginName}] error fetching file:`, error);
+            return null;
+        }
+    }
+
+
+    // Check for updates
+    fetchFirstLine().then(newVersion => {
+        if (newVersion) {
+            if (newVersion !== pluginVersion) {
+                let updateConsoleText = "There is a new version of this plugin available";
+                // Any custom code here
+
+                console.log(`[${pluginName}] ${updateConsoleText}`);
+                setupNotify(pluginVersion, newVersion, pluginName, urlUpdateLink);
+            }
+        }
+    });
+
+    function setupNotify(pluginVersion, newVersion, pluginName, urlUpdateLink) {
+        if (window.location.pathname === '/setup') {
+          const pluginSettings = document.getElementById('plugin-settings');
+          if (pluginSettings) {
+            const currentText = pluginSettings.textContent.trim();
+            const newText = `<a href="${urlUpdateLink}" target="_blank">[${pluginName}] Update available: ${pluginVersion} --> ${newVersion}</a><br>`;
+
+            if (currentText === 'No plugin settings are available.') {
+              pluginSettings.innerHTML = newText;
+            } else {
+              pluginSettings.innerHTML += ' ' + newText;
+            }
+          }
+
+          const updateIcon = document.querySelector('.wrapper-outer #navigation .sidenav-content .fa-puzzle-piece') || document.querySelector('.wrapper-outer .sidenav-content') || document.querySelector('.sidenav-content');
+
+          const redDot = document.createElement('span');
+          redDot.style.display = 'block';
+          redDot.style.width = '12px';
+          redDot.style.height = '12px';
+          redDot.style.borderRadius = '50%';
+          redDot.style.backgroundColor = '#FE0830' || 'var(--color-main-bright)'; // Prefer set colour over theme colour
+          redDot.style.marginLeft = '82px';
+          redDot.style.marginTop = '-12px';
+
+          updateIcon.appendChild(redDot);
+        }
+    }
+}
+
+if (CHECK_FOR_UPDATES) {
+    checkUpdate(
+        pluginSetupOnlyNotify,  // Check only in /setup
+        pluginVersion,          // Plugin version (string)
+        pluginName,             // Plugin name
+        pluginHomepageUrl,      // Update link URL
+        pluginUpdateUrl,        // Update check URL
+    );
+}
